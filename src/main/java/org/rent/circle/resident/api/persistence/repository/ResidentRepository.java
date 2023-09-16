1e9ddef4c94d1473c4e448bee73d584c35e71e1b
matos-nelson <nmatos6@gatech.edu>
2023-09-16 18:32:39 -0500

    Added dto versions of models. Added ResidentRepository.

---
+++ src/main/java/org/rent/circle/resident/api/persistence/repository/ResidentRepository.java
@@ -0,0 +1,8 @@
+package org.rent.circle.resident.api.persistence.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import org.rent.circle.resident.api.persistence.model.Resident;
+
+public class ResidentRepository implements PanacheRepository<Resident> {
+
+}
