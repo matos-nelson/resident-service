3a9de2ca0d278db1d4c32ac2cd307010fb04c614
matos-nelson <nmatos6@gatech.edu>
2024-02-03 11:19:20 -0600

    Added key for manager id in resident schema.

--- db/sql/create_resident_schemas.sql
+++ db/sql/create_resident_schemas.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS resident (
   tenant_id bigint NOT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY manager_id_idx (manager_id),
   KEY property_id_idx (property_id),
   KEY tenant_id_idx (tenant_id),
   UNIQUE KEY user_id_idx (user_id)
